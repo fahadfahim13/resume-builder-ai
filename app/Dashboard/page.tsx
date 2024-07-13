be987983c86d16106b589bf5844b053eef26646b
fahadfahim13 <fahadfahim13@gmail.com>
2024-07-13 13:32:16 +0600

    Adding dashboard and add resume

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
 
 const Dashboard = () => {
   return (
-    <div>
-        
+    <div className="container w-full">
+      <div className="flex justify-between">
+        <h1 className="text-2xl">Dashboard</h1>
+        <Dialog>
+          <DialogTrigger>Create New</DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Are you absolutely sure?</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      </div>
+      <div className="pt-12">
+        <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8">
+          <div className="h-32 rounded-lg bg-gray-200"></div>
+          <div className="h-32 rounded-lg bg-gray-200"></div>
+          <div className="h-32 rounded-lg bg-gray-200"></div>
+          <div className="h-32 rounded-lg bg-gray-200"></div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
